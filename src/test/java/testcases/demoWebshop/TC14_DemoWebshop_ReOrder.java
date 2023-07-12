b91ea61854559d29ff69dad9f0d36b53fd7bc00d
automationcatalogue <automation.catalogue@gmail.com>
2023-07-12 08:32:50 +0530

    TC03 & TC14 testcase adjustments

--- src/test/java/testcases/demoWebshop/TC14_DemoWebshop_ReOrder.java
+++ src/test/java/testcases/demoWebshop/TC14_DemoWebshop_ReOrder.java
@@ -1,84 +1,70 @@
-package tetscases.DemoWebshop;
+package testcases.demoWebshop;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
 
 import java.time.Duration;
 
 public class TC14_DemoWebshop_ReOrder {
-    public static void main(String[] args) throws Exception {
+
+    public static void main(String args[]) throws Exception {
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
 
         driver.get("https://demowebshop.tricentis.com/");
         System.out.println("Demo website is loaded");
-        Thread.sleep(1000);
 
         driver.findElement(By.xpath("//a[@class='ico-login']")).click();
         System.out.println("Login link is clicked");
-        Thread.sleep(1000);
 
         driver.findElement(By.xpath("//input[@id='Email']")).sendKeys("aarosagarch@gmail.com");
         System.out.println("Email ID is entered");
-        Thread.sleep(1000);
+
 
         driver.findElement(By.xpath("//input[@id='Password']")).sendKeys("Admin@123");
         System.out.println("Password is entered");
-        Thread.sleep(1000);
 
         driver.findElement(By.xpath("//input[@class='button-1 login-button']")).click();
-        System.out.println("Login Button is cicked");
-        Thread.sleep(1000);
+        System.out.println("Login Button is clicked");
 
         driver.findElement(By.xpath("//div[@class='master-wrapper-content']//a[@class='account']")).click();
         System.out.println("Email Link is clicked");
-        Thread.sleep(1000);
 
-        driver.findElement(By.xpath("/html/body/div[4]/div[1]/div[4]/div[1]/div/div[2]/ul/li[3]/a")).click();
+        driver.findElement(By.xpath("//div[@class='block block-account-navigation']//ul//a[text()='Orders']")).click();
         System.out.println("Order link is clicked");
-        Thread.sleep(1000);
 
-        driver.findElement(By.xpath("/html/body/div[4]/div[1]/div[4]/div[2]/div/div[2]/div/div[1]/div[2]/input")).click();
+        driver.findElement(By.xpath("//div[@class='order-list']/div[1]//input")).click();
         System.out.println("Order details button is clicked");
-        Thread.sleep(1000);
 
         driver.findElement(By.xpath("//input[@type='button'][@value='Re-order']")).click();
         System.out.println("Re-order button is clicked");
-        Thread.sleep(1000);
 
         driver.findElement(By.xpath("//input[@id='termsofservice']")).click();
         System.out.println("Check-box is clicked");
-        Thread.sleep(2000);
 
         driver.findElement(By.xpath("//button[@value='checkout']")).click();
         System.out.println("Checkout button is clicked");
-        Thread.sleep(2000);
 
         driver.findElement(By.xpath("//input[@onclick='Billing.save()']")).click();
         System.out.println("Continue button is clicked under Billing Address");
-        Thread.sleep(2000);
 
         driver.findElement(By.xpath("//input[@onclick='Shipping.save()']")).click();
         System.out.println("Continue button is clicked under Shipping Address");
-        Thread.sleep(2000);
 
         driver.findElement(By.xpath("//input[@onclick='ShippingMethod.save()']")).click();
         System.out.println("Continue button is clicked under Shipping Method");
-        Thread.sleep(2000);
 
         driver.findElement(By.xpath("//input[@onclick='PaymentMethod.save()']")).click();
         System.out.println("Continue button is clicked under Payment Method");
-        Thread.sleep(2000);
 
         driver.findElement(By.xpath("//input[@onclick='PaymentInfo.save()']")).click();
         System.out.println("Continue button is clicked under Payment Information");
-        Thread.sleep(2000);
 
         driver.findElement(By.xpath("//input[@onclick='ConfirmOrder.save()']")).click();
         System.out.println("Confirm button is clicked under Confirm Order");
-        Thread.sleep(2000);
 
         driver.quit();
 
--- src/test/java/testcases/orangeHRM/TC03_OrangeHRM_AddUser.java
+++ src/test/java/testcases/orangeHRM/TC03_OrangeHRM_AddUser.java
@@ -4,22 +4,28 @@ import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Test;
 import seleniumPrograms.sagarTeachings.JavaScriptExecutorDemo;
 import seleniumPrograms.sagarTeachings.KeysDemo;
 
 import java.time.Duration;
 
 public class TC03_OrangeHRM_AddUser {
-    public static void main(String[] args) throws Exception{
+
+    public static void main(String args[]) throws Exception{
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
-        //Open the Orange HRM Application https://automation41-trials79.orangehrmlive.com
 
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+        String userName="CharlieABCG";
+
+        //Open the Orange HRM Application https://automation41-trials79.orangehrmlive.com
         driver.get("https://testcataloguea-trials79.orangehrmlive.com/auth/login");
 
         //Enter the UserName as "Admin"
         driver.findElement(By.xpath("//input[@id='txtUsername']")).sendKeys("Admin");
+        System.out.println("UserName is Entered");
 
         //Enter the Password as "Admin@123"
         driver.findElement(By.xpath("//input[@id='txtPassword']")).sendKeys("Admin@123");
@@ -27,47 +33,48 @@ public class TC03_OrangeHRM_AddUser {
         //Click on Login button
         driver.findElement(By.xpath("//button[@type='submit']")).click();
 
-        Thread.sleep(6000);
-//Verify the title of a page --> Whether Login is successful or not
+        //Verify the title of a page --> Whether Login is successful or not
         String Orange_title=driver.getTitle();
         String expected_title="Employee Management";
-      if(Orange_title.equalsIgnoreCase(expected_title)){
-          System.out.println("Title is verified");
-
-      }
-      else {
+        if(Orange_title.equalsIgnoreCase(expected_title)){
+              System.out.println("Title is verified");
+        }else {
           System.out.println("Title is not verified");
-      }
-//Click on HR Administration link
+        }
+
+        //Click on HR Administration link
         driver.findElement(By.xpath("(//span[text()='HR Administration'])[1]")).click();
 
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("(//crud-panel[contains(@listdata='systemUsersCtrl.listData.systemUsers']//table//tbody/tr[1]//span[text()])[1]")));
+        //wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("(//crud-panel[contains(@listdata,'systemUsers')]//table//tbody/tr[1]//span[text()])[1]")));
 
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("(//crud-panel[@listdata='systemUsersCtrl.listData.systemUsers']//table//tbody/tr[1]//span[text()])[1]")));
-
-      //Click on + (Add User) Icon
+        //Click on + (Add User) Icon
         driver.findElement(By.xpath("//i[@class='material-icons'][text()='add']")).click();
 
         //Enter Employee Name as Charlie Carter
-        driver.findElement(By.xpath("//input[@id='selectedEmployee_value']")).sendKeys("Charlie Carter");
-        driver.findElement(By.xpath("//input[@id='selectedEmployee_value']")).sendKeys(Keys.TAB);
- //       Enter some Random UserName ( DOUBT)
-        driver.findElement(By.xpath("//input[@id='user_name']")).sendKeys("Charlie12345");
-                //Enter the Password as "Admin@123"
-        driver.findElement(By.xpath("//input[@placeholder='Enter Password']")).sendKeys("Admin@123");
-//Enter the Confirm Password as "Admin@123"
+        driver.findElement(By.xpath("//input[@id='selectedEmployee_value']")).sendKeys("Charlie Car");
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@id='selectedEmployee_dropdown']//div[@class='title-section']")));
+        driver.findElement(By.xpath("//div[@id='selectedEmployee_dropdown']//div[@class='title-section']")).click();
 
+        //Enter some Random UserName
+        driver.findElement(By.xpath("//input[@id='user_name']")).sendKeys(userName);
+
+        //Enter the Password as "Admin@123"
+        driver.findElement(By.xpath("//input[@placeholder='Enter Password']")).sendKeys("Admin@123");
+
+        //Enter the Confirm Password as "Admin@123"
         driver.findElement(By.xpath("//input[@placeholder='Confirm Password']")).sendKeys("Admin@123");
+
         //Click on Save button
         WebElement element_SaveBtn =  driver.findElement(By.xpath("//button[@id='modal-save-button']"));
         JavascriptExecutor js = (JavascriptExecutor)driver;
         js.executeScript("arguments[0].click();",element_SaveBtn);
-        //Click on Logout button
 
+        //Click on Logout button
         driver.findElement(By.xpath("//span[@class='profile-name'][text()='Log Out']")).click();
 
         //Enter the UserName as "Charlie"
-        driver.findElement(By.xpath("//input[@id='txtUsername']")).sendKeys("Charlie12345");
+        driver.findElement(By.xpath("//input[@id='txtUsername']")).sendKeys(userName);
 
         //Enter the Password as "Admin@123"
         driver.findElement(By.xpath("//input[@id='txtPassword']")).sendKeys("Admin@123");
@@ -76,19 +83,16 @@ public class TC03_OrangeHRM_AddUser {
         driver.findElement(By.xpath("//button[@type='submit']")).click();
 
         //Verify the Employee Name as Charlie Carter
-
-        String Employee_Name=driver.findElement(By.xpath("")).getText();
+        String Employee_Name=driver.findElement(By.xpath("//div[@id='sidebar-profile-picture']/a")).getText();
         String expected_name="Charlie Carter";
         if(Employee_Name.equalsIgnoreCase(expected_name)){
-            System.out.println("Nmae is verified");
-
+            System.out.println("UserName is matched and verified");
+        }else {
+            System.out.println("UserName is not matched");
         }
-        else {
-            System.out.println("Name is not verified");
-        }
-//Click on Logout button
 
-        driver.findElement(By.xpath("//span[@class='profile-name']")).click();
+        //Click on Logout button
+        driver.findElement(By.xpath("//span[@class='profile-name'][text()='Log Out']")).click();
 
         driver.quit();
     }
