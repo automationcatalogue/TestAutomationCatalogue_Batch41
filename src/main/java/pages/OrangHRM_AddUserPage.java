ead373d93c293a657426aca87824d69b6220f6be
automationcatalogue <automation.catalogue@gmail.com>
2023-07-19 08:12:54 +0530

    Adding OrangeHRM AddUser webpage

---
+++ src/main/java/pages/OrangHRM_AddUserPage.java
@@ -0,0 +1,7 @@
+package pages;
+
+import org.openqa.selenium.By;
+
+public class OrangHRM_AddUserPage {
+    public static By txtbx_EmployeeName = By.xpath("//input[@id='selectedEmployee_value']");
+}
