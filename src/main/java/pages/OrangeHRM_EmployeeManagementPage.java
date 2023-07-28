3946b795eff7d86d6ec41b0efbf66a0cebc02a4f
ramyaseltest <ramya.seltest8429@gmail.com>
2023-07-27 21:14:35 -0400

    Employee management page and Homepage is created.

--- src/main/java/pages/OrangeHRM_EmployeeManagementPage.java
+++ src/main/java/pages/OrangeHRM_EmployeeManagementPage.java
@@ -5,4 +5,7 @@ import org.openqa.selenium.By;
 public class OrangeHRM_EmployeeManagementPage {
     public static By Link_FirstEmployee = By.xpath("//tr[contains(@ng-repeat,'employee.empNumber')][1]/td[2]");
     public static By Link_PersonalDetails = By.xpath("//a[contains(@ui-sref,'l_details')]");
+    public static By link_EmployeeList =By.xpath("//a[text()='Employee List ']");
+    public static By list_employee= By.xpath("//table[@id='employeeListTable']/tbody/tr/td[3]");
+    public static By txt_supervisorName=By.xpath(".//following-sibling::td[6]");
 }
