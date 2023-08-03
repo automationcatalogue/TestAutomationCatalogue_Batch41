ade8a825e2f941f54c2a0dd7af9d14e46b439808
myinnovativeprojectsSarika <sarika0216@gmail.com>
2023-08-03 14:17:32 -0400

    Added changes to my TC03

---
+++ src/test/java/testcases/PseudoTests/TC10_DemoWebShop_CreateAddress.java
@@ -0,0 +1,120 @@
+package testcases.PseudoTests;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+import utilities.CommonUtils;
+
+import java.time.Duration;
+import java.util.List;
+
+public class TC10_DemoWebShop_CreateAddress {
+
+    public static void main(String[] args) throws Exception{
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+
+        driver.manage().window().maximize();
+        System.out.println("Chrome browser window maximized");
+
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+
+        driver.get("https://demowebshop.tricentis.com/");
+        System.out.println("DemoWebShop Website is launched");
+
+        driver.findElement(By.xpath("//a[text()='Log in']")).click();
+        System.out.println("clicked on login button");
+
+        driver.findElement(By.cssSelector("#Email")).sendKeys("aarosagarch@gmail.com");
+        System.out.println("Email ID is entered");
+
+        driver.findElement(By.cssSelector(".password")).sendKeys("Admin@123");
+        System.out.println("Password is entered");
+
+        driver.findElement(By.xpath("//input[contains(@class,'login-button')]")).click();
+        System.out.println("Clicked on login button");
+
+        driver.findElement(By.xpath("(//a[@href='/customer/info'])[1]")).click();
+        System.out.println("clicked on the email.id");
+
+        driver.findElement(By.xpath("(//a[text()='Addresses'])[1]")).click();
+        System.out.println("clicked on adresses ");
+
+        driver.findElement(By.xpath("//input[@value='Add new']")).click();
+        System.out.println("Clicked on the Add New Button");
+
+        driver.findElement(By.cssSelector("#Address_FirstName")).sendKeys("Rama");
+        System.out.println("Entered Random First Name");
+
+        driver.findElement(By.cssSelector("#Address_LastName")).sendKeys("Krishna");
+        System.out.println("Entered Random last name");
+
+        driver.findElement(By.cssSelector("#Address_Email")).sendKeys("ram@gmail.com");
+        System.out.println("Entered Random Email");
+
+        driver.findElement(By.cssSelector("#Address_Company")).sendKeys("Amazon");
+        System.out.println("Entered Random company name");
+
+        WebElement element_Country = driver.findElement(By.xpath("//select[@id='Address_CountryId']"));
+        Select select_Country = new Select(element_Country);
+        select_Country.selectByVisibleText("India");
+        System.out.println("India is selected from the Country drop-down");
+
+        driver.findElement(By.cssSelector("#Address_City")).sendKeys("Hyderabad");
+        System.out.println("Entered Random city");
+
+        driver.findElement(By.cssSelector("#Address_Address1")).sendKeys("Uppal");
+        System.out.println("Entered Random Address1");
+
+        driver.findElement(By.cssSelector("#Address_Address2")).sendKeys("Ramanthapur");
+        System.out.println("Entered Random Address2");
+
+        driver.findElement(By.cssSelector("#Address_ZipPostalCode")).sendKeys("500013");
+        System.out.println("Entered Random Zip code");
+
+        driver.findElement(By.cssSelector("#Address_PhoneNumber")).sendKeys("8688881838");
+        System.out.println("Entered Random Phone number");
+
+        driver.findElement(By.cssSelector("#Address_FaxNumber")).sendKeys("12354");
+        System.out.println("Entered Fax Number");
+
+        driver.findElement(By.xpath("//input[@value='Save']")).click();
+        System.out.println("Clicked on Save Button");
+
+        driver.findElement(By.xpath("(//a[text()='Addresses'])[1]")).click();
+        System.out.println("Clicked on Address link");
+
+        List<WebElement> address_list = driver.findElements(By.xpath("//div[contains(@class,'st-page')]/div[2]/div[1]/div/div[1]"));
+
+        boolean isAddressFound = false;
+        for(WebElement name:address_list){
+            String present_name=name.getText();
+            if(present_name.equalsIgnoreCase("Rama Krishna")){
+                isAddressFound=true;
+                break;
+            }
+        }
+
+        if(isAddressFound){
+            System.out.println("Address Creation is successful");
+        }else{
+            System.out.println("Address Creation is not successful");
+        }
+
+        driver.findElement(By.xpath("//a[text()='Log out']")).click();
+        System.out.println("Clicked on Log out Button");
+
+        Thread.sleep(6000);
+
+        driver.quit();
+        System.out.println("Browser is closed");
+
+    }
+}
---
+++ src/test/java/testcases/demoWebshop/TC11_DemoWebshop_ReOrder.java
@@ -0,0 +1,143 @@
+package testcases.demoWebshop;
+
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.*;
+import pages.*;
+import utilities.CommonUtils;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.time.Duration;
+
+public class TC11_DemoWebshop_ReOrder {
+
+    static FileInputStream fis;
+    static XSSFWorkbook wbk;
+    static XSSFSheet sh;
+    static XSSFRow row;
+    static XSSFCell cell_userName;
+    static String userName;
+    static XSSFCell cell_password;
+    static String password;
+    static XSSFCell cell_OrderNumber;
+    static String orderNumber;
+    static FileOutputStream fos;
+    static String projectPath;
+
+    @BeforeClass
+    public void prerequisite_setup() throws Exception {
+        projectPath = System.getProperty("user.dir");
+        fis = new FileInputStream(projectPath+"\\src\\main\\resources\\AutomationCatalogue_Batch41_TestData.xlsx");
+        wbk = new XSSFWorkbook(fis);
+        sh = wbk.getSheet("DemoWebshop_ReOrder");
+        row = sh.getRow(1);
+
+        cell_userName = row.getCell(3);
+        userName = cell_userName.getStringCellValue();
+        System.out.println("UserName from excel sheet is :" + userName);
+
+        cell_password = row.getCell(4);
+        password = cell_password.getStringCellValue();
+        System.out.println("Password from excel sheet is:" + password);
+
+
+    }
+
+    @Test
+    @Parameters({"browserName"})
+    public static void ReOrder(@Optional("chrome") String browserName) throws Exception {
+
+        WebDriver driver = CommonUtils.browserLaunch(browserName);
+
+        driver.get("https://demowebshop.tricentis.com/");
+        System.out.println("Demo website is loaded");
+
+        driver.findElement(DemoWebshop_HomePage.link_Login).click();
+        System.out.println("Login link is clicked");
+
+        driver.findElement(DemoWebshop_LoginPage.txtbx_UserName).sendKeys(userName);
+        System.out.println("Email ID is entered");
+
+        driver.findElement(DemoWebshop_LoginPage.txtbx_Password).sendKeys(password);
+        System.out.println("Password is entered");
+
+        driver.findElement(DemoWebshop_LoginPage.btn_Login).click();
+        System.out.println("Login Button is clicked");
+
+        driver.findElement(DemoWebshop_HomePage.link_Email).click();
+        System.out.println("Email Link is clicked");
+
+        driver.findElement(DemoWebShop_OrdersPage.link_Orders).click();
+        System.out.println("Order link is clicked");
+
+        driver.findElement(DemoWebShop_OrdersPage.btn_OrderDetails).click();
+        System.out.println("Order details button is clicked");
+
+        driver.findElement(DemoWebshop_OrderInformationPage.btn_Reorder).click();
+        System.out.println("Re-order button is clicked");
+
+        driver.findElement(DemoWebshop_CartPage.checkbox_Iagree).click();
+        System.out.println("Check-box is clicked");
+
+        driver.findElement(DemoWebshop_CartPage.btn_Checkout).click();
+        System.out.println("Checkout button is clicked");
+
+        driver.findElement(DemoWebshop_CheckoutPage.btn_BillingContinue).click();
+        System.out.println("Continue button is clicked under Billing Address");
+
+        driver.findElement(DemoWebshop_CheckoutPage.btn_ShippingAddress).click();
+        System.out.println("Continue button is clicked under Shipping Address");
+
+        driver.findElement(DemoWebshop_CheckoutPage.btn_ShippingMethod).click();
+        System.out.println("Continue button is clicked under Shipping Method");
+
+        driver.findElement(DemoWebshop_CheckoutPage.btn_PaymentMethod).click();
+        System.out.println("Continue button is clicked under Payment Method");
+
+        driver.findElement(DemoWebshop_CheckoutPage.btn_PaymentInformation).click();
+        System.out.println("Continue button is clicked under Payment Information");
+
+        driver.findElement(DemoWebshop_CheckoutPage.btn_ConfirmOrder).click();
+        System.out.println("Confirm button is clicked under Confirm Order");
+
+        WebElement element_OrderNumber = driver.findElement(DemoWebshop_CheckoutPage.txt_OrderNumber);
+        if (element_OrderNumber.isDisplayed()) {
+            orderNumber = element_OrderNumber.getText();
+            System.out.println("order number is generated " + orderNumber);
+        } else {
+            System.out.println("OrderNumber is not generated");
+        }
+
+        driver.findElement(DemoWebshop_HomePage.btn_Logout).click();
+        System.out.println("log out is clicked");
+
+        driver.close();
+
+    }
+
+    @AfterClass
+    public void tearDown() throws Exception {
+
+        cell_OrderNumber = row.getCell(5);
+        if(cell_OrderNumber==null){
+            cell_OrderNumber=row.createCell(5);
+        }
+        cell_OrderNumber.setCellValue(orderNumber);
+        fos = new FileOutputStream(projectPath+"\\src\\main\\resources\\AutomationCatalogue_Batch41_TestData.xlsx");
+        wbk.write(fos);
+        System.out.println(orderNumber+" is written back to the Excel file");
+
+        fos.close();
+        System.out.println("ExcelFile Writing is closed");
+        fis.close();
+        System.out.println("ExcelFile reading is closed");
+    }
+}
