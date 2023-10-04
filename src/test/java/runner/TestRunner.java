681209d33a7809368cd120142264bc6f776dedef
HarithaVuppu <harita.vuppugala>
2023-10-03 20:50:01 -0400

    BDD implementation

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -4,7 +4,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
-        tags = " @OrangeHRM",
+        tags = "@TotalOrders",
         features = "src/test/resources/",
         glue = "stepdefinitions",
         dryRun = false
